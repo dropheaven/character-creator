81c6bc98b87682a80dd0e96ae38a37318692d904
Saad Ahmed <sazero@hey.com>
2023-09-06 14:09:04 -0400

    workshop complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          top: "60%",
+          height: "100%",
+          width: "100%",
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
